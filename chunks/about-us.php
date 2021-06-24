b33544bc8b5cc6da982c3a403c022e99733c0655
Utkarsh-Patni <76656242+Utkarsh-Patni@users.noreply.github.com>
2021-06-24 21:05:08 +0530

    Add files via upload

---
+++ chunks/about-us.php
@@ -0,0 +1,7 @@
+<section class="fabout">
+		<div class="section white center">
+		      <div class="row container">
+		        <h2 class="header">ABOUT US</h2>
+		        <p class="grey-text text-darken-3 lighten-3">Since our modest beginnings in 2019 with a little space in City’s stylish Area, <em> RestroDigital‘</em>s development has been enlivened with the energy to cook and serve solid, Indian-roused takeout food. In contrast to other Indian eateries, Restrodigital was made with the explicit expectation to appear as something else. We realize numerous individuals love Indian sustenance, yet a large number of them loathe or are unconscious of the regularly unfortunate fixings that make run of the mill Indian nourishment taste so great. Our menu highlights things that utilization the sound and fragrant flavors, however forgets the stuffing ghee, spread, oil, and overwhelming cream.</p>
+		      </div>
+	</section>
---
+++ chunks/description.php
@@ -0,0 +1,13 @@
+<section class="fdes">
+		<div class="section white center">
+			<h2 class="header" style="padding:20px; padding-bottom: 30px;">Resturant Powered By Digital</h2>
+		      <div class="row container center">
+		        <div class="col center l8 s12">
+		        	<p>Consumer demands for convenience and seamless digital experiences are on the rise, further accelerated by the COVID-19 pandemic. How can food establishments elevate customer experiences with the help of new convenience practices and digital ecosystems to be successful with the next-generation guest? We serve up insights into three trends that are shaping consumption patterns among restaurant guests and examine actions restaurants of the future can take to thrive.</p>
+		        </div>
+		        <div class="col center l4 s12">
+		        	<img height="300" width="auto" style="object-fit: contain;" src=".\images\17964.jpg" alt="">
+		        </div>
+		        
+		      </div>
+	</section>
